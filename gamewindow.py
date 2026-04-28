28aba4c1c64934ad4288d868bcdc1028dea59ee5
TheHudat <jhudakjh@gmail.com>
2026-04-28 15:56:18 -0400

    initial commit

---
+++ gamewindow.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk
+from constants import *
+
+class GameWindow:
+    def __init__(self, root):
+        root.title("Wordle")
+
+        mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.generate_square_row(mainframe, 0)
+        self.generate_square_row(mainframe, 1)
+        self.generate_square_row(mainframe, 2)
+        self.generate_square_row(mainframe, 3)
+        self.generate_square_row(mainframe, 4)
+        self.generate_square_row(mainframe, 5)
+
+    
+    def generate_square_row(self, parent, row_number):
+        letter_square = ttk.Frame(parent, width = GAME_SQUARE_WIDTH, height = GAME_SQUARE_HEIGHT, borderwidth = 5, relief="ridge")
+        letter_square2 = ttk.Frame(parent, width = GAME_SQUARE_WIDTH, height = GAME_SQUARE_HEIGHT, borderwidth = 5, relief="ridge")
+        letter_square3 = ttk.Frame(parent, width = GAME_SQUARE_WIDTH, height = GAME_SQUARE_HEIGHT, borderwidth = 5, relief="ridge")
+        letter_square4 = ttk.Frame(parent, width = GAME_SQUARE_WIDTH, height = GAME_SQUARE_HEIGHT, borderwidth = 5, relief="ridge")
+        letter_square5 = ttk.Frame(parent, width = GAME_SQUARE_WIDTH, height = GAME_SQUARE_HEIGHT, borderwidth = 5, relief="ridge")
+
+        letter_square.grid(column = 0, row = row_number, sticky=(E, N))
+        letter_square2.grid(column = 1, row = row_number, sticky=(E, N))
+        letter_square3.grid(column = 2, row = row_number, sticky=(E, N))
+        letter_square4.grid(column = 3, row = row_number, sticky=(E, N))
+        letter_square5.grid(column = 4, row = row_number, sticky=(E, N))
---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+from gamewindow import GameWindow
+
+
+root = Tk()
+GameWindow(root)
+root.mainloop()
+
