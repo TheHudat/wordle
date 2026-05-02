af957a24087bcc51ccb9a342057b282e65f13228
TheHudat <jhudakjh@gmail.com>
2026-05-02 12:37:07 -0400

    added backspace and enter functionality

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ class Wordle(Tk):
         self.title("Wordle")
         self.active_row = 0
         self.active_column = 0
+        self.word_entry = ""
         
         #initiate the window and display the background      
         self.game_window = ttk.Frame(self, padding=(3, 3, 5, 5))
@@ -35,17 +36,38 @@ class Wordle(Tk):
             squares.append(square_row)
         return squares
     
-    def button_input(self, event):
+    def on_letter_key(self, event):
         letter = event.char.capitalize()
         row = self.active_row
         column = self.active_column
         if letter in LETTERS:
             self.squares[row][column].letter_text.set(letter)
+            self.word_entry += letter
         
             #move to next active input square
             if column < 4:
                 self.active_column += 1
-            else:
-                self.squares[row][column].set_square_color()
-                self.active_row += 1
-                self.active_column = 0
+    
+    def on_backspace(self):
+        row = self.active_row
+        column = self.active_column
+        if self.active_column == 4 and self.squares[row][column].letter_text.get() != '':
+            self.squares[row][column].letter_text.set('')
+            return
+        if self.active_column > 0:
+            self.active_column -= 1
+            self.squares[row][self.active_column].letter_text.set('')
+
+    def on_enter(self):
+        #only on a full word
+        row = self.active_row
+        column = self.active_column
+        if column == 4 and self.squares[row][column].letter_text.get() != '':
+            #set colors on each square in row
+            for col in range(5):
+                self.squares[self.active_row][col].set_square_color()
+            
+            #set next row as active
+            self.active_column = 0
+            self.active_row +=1
+        
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from game import Wordle
 
 def main():
     game = Wordle()
-    game.bind("<Key>", game.button_input)
+    game.bind("<Key>", game.on_letter_key)
+    game.bind("<BackSpace>", lambda event: game.on_backspace())
+    game.bind("<Return>", lambda event: game.on_enter())
     game.mainloop()
 
 if __name__ == "__main__":
